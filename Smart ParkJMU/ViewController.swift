b35b80b3255d12a2b0ec2c371076856c4bd3fb8c
Riley Sung <rsung@techdynamism.com>
2015-11-09 16:32:21 -0500

    Initial Commit

---
+++ Smart ParkJMU/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart ParkJMU
+//
+//  Created by Riley Sung on 11/9/15.
+//  Copyright © 2015 Riley Sung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
